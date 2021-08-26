8943b74b06053f391b0bd9081d350bb4ff34b33a
Agustin Ferrante <agus_p3@hotmail.com>
2021-08-26 06:03:26 -0300

    Añadido javadoc a la clase student e implementada la clase launcher

--- src/entities/Student.java
+++ src/entities/Student.java
@@ -9,6 +9,15 @@ public class Student {
 	private String githubURL;
 	private String pathPhoto;
 	
+	/**
+	 * Creates a new student.
+	 * @param id The new student's university id.
+	 * @param lastName The new student's last name.
+	 * @param firstName The new student's first name.
+	 * @param mail The new student's e-mail.
+	 * @param githubURL The new student's GitHub profile URL.
+	 * @param pathPhoto The new student's photo path.
+	 */
 	public Student(int id, String lastName, String firstName, String mail, String githubURL, String pathPhoto) {
 		this.id =id;
 		this.lastName = lastName;
@@ -18,50 +27,98 @@ public class Student {
 		this.pathPhoto = pathPhoto;
 	}
 	
+	/**
+	 * Returns the student's id.
+	 * @return The student's id
+	 */
 	public int getId() {
 		return id;
 	}
 	
+	/**
+	 * Returns the student's last name.
+	 * @return The student's last name.
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 	
+	/**
+	 * Returns the student's first name.
+	 * @return The student's first name.
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
 	
+	/**
+	 * Returns the student's e-mail.
+	 * @return The student's e-mail.
+	 */
 	public String getMail() {
 		return mail;
 	}
 	
+	/**
+	 * Returns the student's GitHub profile URL.
+	 * @return The student's GitHub profile URL.
+	 */
 	public String getGithubURL() {
 		return githubURL;
 	}
 	
+	/**
+	 * Returns the student's photo path.
+	 * @return The student's photo path.
+	 */
 	public String getPathPhoto() {
 		return pathPhoto;
 	}
 	
+	/**
+	 * Sets the student's id.
+	 * @param id The new id for this student.
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 	
+	/**
+	 * Sets the student's last name.
+	 * @param lastName The new last name for this student.
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 	
+	/**
+	 * Sets the student's first name.
+	 * @param firstName The new first name for this student.
+	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 	
+	/**
+	 * Sets the student's mail.
+	 * @param mail The new e-mail for this student.
+	 */
 	public void setMail(String mail) {
 		this.mail = mail;
 	}
 	
+	/**
+	 * Sets the student's GitHub profile URL.
+	 * @param githubURL The new GitHub profile URL for this student.
+	 */
 	public void setGithubURL(String githubURL) {
 		this.githubURL = githubURL;
 	}
 	
+	/**
+	 * Sets the student's photo path.
+	 * @param pathPhoto The new path to this student's photo.
+	 */
 	public void setPathPhoto(String pathPhoto) {
 		this.pathPhoto = pathPhoto;
 	}
--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,13 +1,20 @@
 package launcher;
 
+import entities.Student;
+import gui.SimplePresentationScreen;
+
 public class Launcher {
+	
 	public static void main(String [] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-            	
-            }
-        });
-    }
+		
+		//Schedule a job for the event-dispatching thread:
+		//creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				SimplePresentationScreen frame = new SimplePresentationScreen(new Student(120314, "Ferrante", "Agustin Pedro", "agus_p3@hotmail.com",
+						"https://github.com/AgusF22", null));
+				frame.setVisible(true);
+			}
+		});
+	}
 }
